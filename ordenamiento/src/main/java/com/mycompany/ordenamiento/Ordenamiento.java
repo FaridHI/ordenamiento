c9c60e2467c5b780e100ef7a5c6134a8445c8feb
FaridHI <1121041@senati.pe>
2023-08-17 19:26:51 -0500

    agregue decoracion

--- ordenamiento/src/main/java/com/mycompany/ordenamiento/Ordenamiento.java
+++ ordenamiento/src/main/java/com/mycompany/ordenamiento/Ordenamiento.java
@@ -16,7 +16,9 @@ public class Ordenamiento {
         
         
         for (int i = 0; i < numeros.length; i++){
+            System.out.println("-------------------");
             System.out.println("Ingrese el numero: ");
+            System.out.println("-------------------");
             numeros[i] = scanner.nextInt();
         }
         
@@ -30,6 +32,7 @@ public class Ordenamiento {
             }
         }
         
+        System.out.println("-----------------------------------");
         System.out.println("Numeros ordenados de menor a mayor:");
         for (int numero : numeros){
             System.out.print(numero + " ");
